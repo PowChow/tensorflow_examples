cb2d6f8398eb1ebc7023ab82927ce5983c916f52
PowChow <pauline.chow@gmail.com>
2016-07-28 21:03:43 -0700

    update

--- install_conda2.sh
+++ install_conda2.sh
@@ -22,4 +22,4 @@ sudo apt-get install libsm6 libxrender1 libfontconfig1
 conda install matplotlib
 curl https://bootstrap.pypa.io/ez_setup.py -o - | python #gets easy_install
 export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.9.0-cp27-none-linux_x86_64.whl
-pip install --upgrade $TF_BINARY_URL
+sudo pip install --upgrade $TF_BINARY_URL
